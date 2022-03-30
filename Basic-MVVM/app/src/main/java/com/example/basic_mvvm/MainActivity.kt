a020ce15f415472f25b195bdaa5a6b94ffc740d7
k906506 <k906506@naver.com>
2022-03-30 13:54:23 +0900

    🏗️ [feat] 2. DataBinding 로 변경해서 구현

--- Basic-MVVM/app/src/main/java/com/example/basic_mvvm/MainActivity.kt
+++ Basic-MVVM/app/src/main/java/com/example/basic_mvvm/MainActivity.kt
@@ -2,18 +2,19 @@ package com.example.basic_mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.basic_mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 
-    private var num : Int = 0
+    private var num: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
         bindViews()
         increase()
