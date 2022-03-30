ce21876a87c9b704362cb2d18fe77f50f8c2c72d
k906506 <k906506@naver.com>
2022-03-30 13:39:31 +0900

    🏗️ [feat] 1. ViewBinding 만 사용해서 구현

--- Basic-MVVM/app/src/main/java/com/example/basic_mvvm/MainActivity.kt
+++ Basic-MVVM/app/src/main/java/com/example/basic_mvvm/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.basic_mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.basic_mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private var num : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        bindViews()
+        increase()
+        decrease()
+    }
+
+    private fun bindViews() {
+        binding.displayNumberTextView.text = num.toString()
+    }
+
+    private fun increase() {
+        binding.plusButton.setOnClickListener {
+            num += 1
+            binding.displayNumberTextView.text = num.toString()
+        }
+    }
+
+    private fun decrease() {
+        binding.minusButton.setOnClickListener {
+            num -= 1
+            binding.displayNumberTextView.text = num.toString()
+        }
     }
 }
