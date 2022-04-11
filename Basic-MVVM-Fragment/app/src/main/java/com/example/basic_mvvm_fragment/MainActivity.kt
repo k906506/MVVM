57653c7e25cfda7b99f4f738601d409661dab364
k906506 <k906506@naver.com>
2022-04-11 22:37:05 +0900

    🏗️ [feat] Fragment에서 ViewModel 사용을 위해 Activity와 ViewModel 연결

--- Basic-MVVM-Fragment/app/src/main/java/com/example/basic_mvvm_fragment/MainActivity.kt
+++ Basic-MVVM-Fragment/app/src/main/java/com/example/basic_mvvm_fragment/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.basic_mvvm_fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import com.example.basic_mvvm_fragment.databinding.ActivityMainBinding
+import com.example.basic_mvvm_fragment.viewmodel.FragmentViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(FragmentViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.fragmentViewModel = viewModel
+        binding.lifecycleOwner = this
     }
 }
